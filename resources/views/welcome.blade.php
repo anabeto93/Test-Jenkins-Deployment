bd245386d373969c65dcdcf73ab7fd2377c1ff24
anabeto93 <anabeto93@gmail.com>
2022-09-14 19:16:42 +0100

    assert true. 8088 taken too?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Port 8087 Deployment
+                                    Port 8088 Deployment
                                 </div>
                             </div>
                         </div>
@@ -81,7 +81,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    8086 was already taken. driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.
+                                    8088 was already taken. driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.
                                 </div>
                             </div>
                         </div>
